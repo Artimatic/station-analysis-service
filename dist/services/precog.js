c14946c066aa70d540f164e03f19c6ba1de85d85
DESKTOP-4SRJMOF\dvxch <dvxchan@gmail.com>
2018-05-12 15:46:34 -0400

    Testing out brain.js

--- dist/services/precog.js
+++ dist/services/precog.js
@@ -1,61 +1,35 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const recurrent_js_1 = require("recurrent-js");
+const brain = __importStar(require("brain.js"));
 class Precog {
     constructor() { }
     train() {
-        // const test = [
-        //     0,
-        //     Math.sin(3.14 / 2),
-        //     Math.sin(3.14 / 2),
-        //     Math.sin(3.14 / 2),
-        //     Math.sin(3.14 / 2),
-        //     Math.sin(3.14 / 2),
-        // ];
-        // define network structure
-        const netOpts = {
-            inputSize: 3,
-            hiddenUnits: [6, 2, 6],
-            outputSize: 4
-        };
-        const net = new recurrent_js_1.RNN(netOpts);
-        // instantiate a graph with backprop-ability
-        const graph = new recurrent_js_1.Graph(true);
-        let decision = undefined, previousDecision = undefined;
-        for (let i = 0, end = test.length; i < end; i++) {
-            previousDecision = decision;
-            /*
-            1. Before forward pass:
-            Create a single row of type `Mat`, which holds an observation.
-            We will refer to this as the state.
-            Dimensions of state are according to the configuration of the net (rows = inputSize = 3, cols = 1).
-            */
-            const observation = [1, 0, 1]; /* an observation (ideally normalized) */
-            const state = new recurrent_js_1.Mat(3, 1);
-            state.setFrom(observation);
-            /*
-            2. Decision making:
-            Forward pass with observed state of type `Mat` and graph.
-            The resulting decision is of type `Mat` and is holding multiple output values (here: 4).
-            */
-            decision = net.forward(state, previousDecision, graph);
-            /*
-            3. After forward pass:
-            Compute the decision errors.
-            We refer to this as the loss value(s).
-            Inject that loss value into the derivative of your targeted value.
-            Here you could also apply e.g. loss clipping before injecting the value.
-            NOTE: You can also apply multiple loss values, to the respective array fields.
-            */
-            // decision.output.dw[1] = 0.5;
-            console.log("loss value: ", decision.output.dw[1]);
-            /*
-            4. After injecting the loss value:
-            since graph is keeping a reference of `decision`, it can now perform the backpropagation and therefore adjust the decisions gradient.
-            */
-            graph.backward();
-            /* REPEAT numbers 1 to 5 till the loss value(s) reach a certain threshold */
+        // fibonacci
+        const fibonacci = [
+            { input: [1], output: [1] },
+            { input: [2], output: [1] }
+        ];
+        for (let i = 2; i <= 50; i++) {
+            const tmp = fibonacci.slice(-2);
+            fibonacci.push({ input: [i], output: [(tmp[0].output[0] + tmp[1].output[0])] });
         }
+        const net = new brain.recurrent.LSTM();
+        net.train(fibonacci.slice(0, -1), {
+            errorThresh: 0.005,
+            iterations: 200,
+            log: true,
+            logPeriod: 10,
+            learningRate: 0.3 // learning rate
+        });
+        console.log(fibonacci);
+        return net.run([51]);
     }
 }
 exports.default = new Precog();
--- src/services/precog.ts
+++ src/services/precog.ts
@@ -1,72 +1,34 @@
-import { Graph, Mat, InnerState, RNN } from "recurrent-js";
+import * as brain from 'brain.js';
 
 class Precog {
     constructor() { }
 
-    public train(): void {
+    public train(): any {
+        // fibonacci
+        const fibonacci = [
+            { input: [1], output: [1] },
+            { input: [2], output: [1] }
+        ];
 
-        // const test = [
-        //     0,
-        //     Math.sin(3.14 / 2),
-        //     Math.sin(3.14 / 2),
-        //     Math.sin(3.14 / 2),
-        //     Math.sin(3.14 / 2),
-        //     Math.sin(3.14 / 2),
+        for (let i = 2; i <= 50; i++) {
+            const tmp = fibonacci.slice(-2);
 
-        // ];
-
-        // define network structure
-        const netOpts = {
-            inputSize: 3,
-            hiddenUnits: [ 6, 2, 6 ],
-            outputSize: 4
-          };
-
-        const net = new RNN(netOpts);
-
-        // instantiate a graph with backprop-ability
-        const graph = new Graph(true);
-
-        let decision = undefined,
-            previousDecision = undefined;
-
-        for (let i = 0, end = test.length; i < end; i++) {
-            previousDecision = decision;
-            /*
-            1. Before forward pass:
-            Create a single row of type `Mat`, which holds an observation.
-            We will refer to this as the state.
-            Dimensions of state are according to the configuration of the net (rows = inputSize = 3, cols = 1).
-            */
-            const observation = [1, 0, 1]; /* an observation (ideally normalized) */
-            const state = new Mat(3, 1);
-            state.setFrom(observation);
-
-            /*
-            2. Decision making:
-            Forward pass with observed state of type `Mat` and graph.
-            The resulting decision is of type `Mat` and is holding multiple output values (here: 4).
-            */
-            decision = net.forward(state, previousDecision, graph);
-
-            /*
-            3. After forward pass:
-            Compute the decision errors.
-            We refer to this as the loss value(s).
-            Inject that loss value into the derivative of your targeted value.
-            Here you could also apply e.g. loss clipping before injecting the value.
-            NOTE: You can also apply multiple loss values, to the respective array fields.
-            */
-            // decision.output.dw[1] = 0.5;
-            console.log("loss value: ", decision.output.dw[1]);
-            /*
-            4. After injecting the loss value:
-            since graph is keeping a reference of `decision`, it can now perform the backpropagation and therefore adjust the decisions gradient.
-            */
-            graph.backward();
-
-            /* REPEAT numbers 1 to 5 till the loss value(s) reach a certain threshold */
+            fibonacci.push({ input: [i], output: [(tmp[0].output[0] + tmp[1].output[0])] });
         }
+
+        const net = new brain.recurrent.LSTM();
+
+        net.train(fibonacci.slice(0, -1), {
+            errorThresh: 0.005,  // error threshold to reach
+            iterations: 200,   // maximum training iterations
+            log: true,           // console.log() progress periodically
+            logPeriod: 10,       // number of iterations between logging
+            learningRate: 0.3    // learning rate
+        });
+
+        console.log(fibonacci);
+
+        return net.run([51]);
     }
 }
 
