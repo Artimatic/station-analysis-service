c408db72d18ddca84bbbf0dfa8206671ab8664ee
David Chan <dvxchan@gmail.com>
2019-12-20 05:04:06 -0500

    Removed mongoose dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,12 @@
 import express from 'express';
 import compression from 'compression';  // compresses requests
-import session from 'express-session';
 import bodyParser from 'body-parser';
-import logger from './util/logger';
 import lusca from 'lusca';
 import dotenv from 'dotenv';
-import mongo from 'connect-mongo';
 import flash from 'express-flash';
 import path from 'path';
-import mongoose from 'mongoose';
 import passport from 'passport';
 import expressValidator from 'express-validator';
-import bluebird from 'bluebird';
-import { MONGODB_URI, SESSION_SECRET } from './util/secrets';
-
-const MongoStore = mongo(session);
 
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv.config({ path: '.env.example' });
@@ -25,16 +17,6 @@ import * as apiController from './controllers/api';
 // Create Express server
 const app = express();
 
-// Connect to MongoDB
-const mongoUrl = MONGODB_URI;
-(<any>mongoose).Promise = bluebird;
-mongoose.connect(mongoUrl, {useMongoClient: true}).then(
-  () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-).catch(err => {
-  console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
-  // process.exit();
-});
-
 // Express configuration
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '../views'));
@@ -43,15 +25,7 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: SESSION_SECRET,
-  store: new MongoStore({
-    url: mongoUrl,
-    autoReconnect: true
-  })
-}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
