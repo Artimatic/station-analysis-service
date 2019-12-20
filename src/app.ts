583ac58ade5b7a7d883aeed4a37882a9d53a5a3d
David Chan <dvxchan@gmail.com>
2019-12-20 05:19:39 -0500

    Fixed undefined

--- src/app.ts
+++ src/app.ts
@@ -36,17 +36,6 @@ app.use((req, res, next) => {
   next();
 });
 app.use((req, res, next) => {
-  // After successful login, redirect back to the intended page
-  if (!req.user &&
-    req.path !== '/login' &&
-    req.path !== '/signup' &&
-    !req.path.match(/^\/auth/) &&
-    !req.path.match(/\./)) {
-    req.session.returnTo = req.path;
-  } else if (req.user &&
-    req.path == '/account') {
-    req.session.returnTo = req.path;
-  }
   next();
 });
 
